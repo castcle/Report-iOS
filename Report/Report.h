06e27094fca66736fe4031f2ee39cbeab4612243
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2022-08-05 17:15:35 +0700

    Init project

---
+++ Report/Report.h
@@ -0,0 +1,18 @@
+//
+//  Report.h
+//  Report
+//
+//  Created by Tanakorn Phoochaliaw on 5/8/2565 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Report.
+FOUNDATION_EXPORT double ReportVersionNumber;
+
+//! Project version string for Report.
+FOUNDATION_EXPORT const unsigned char ReportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Report/PublicHeader.h>
+
+
